b3577e08fb6dddc9ecaf1c4642adb1a051bcaa7a
AdrianaTM99 <72172104+AdrianaTM99@users.noreply.github.com>
2026-02-11 19:57:22 -0400

    Update app_caudalimetro.py

--- app_caudalimetro.py
+++ app_caudalimetro.py
@@ -114,7 +114,7 @@ div[data-testid="stSlider"] [role="slider"] {
     margin: 0 auto !important;
     padding: 200px 2rem 4rem 2rem !important;
     color: white !important;
-    font-size: 2rem !important;
+    font-size: 1.5rem !important;
 
 }
 /* RESPONSIVE TÍTULO */
@@ -382,6 +382,7 @@ if st.button('🚀 Generar curva de calibración'):
 
 st.write("---")
 st.caption("Adriana Teixeira Mendoza - Universidad Central de Venezuela - 2026")
+
 
 
 
