67178ee6a982129b6b68a6b071472eb200b43107
AdrianaTM99 <72172104+AdrianaTM99@users.noreply.github.com>
2026-05-08 22:05:24 -0400

    Update app_caudalimetro.py

--- app_caudalimetro.py
+++ app_caudalimetro.py
@@ -498,8 +498,7 @@ En un caudalímetro electromagnético, el diámetro interno influye directamente
                     """)
     st.markdown("---")
     with st.expander('información sobre el **modo realista**', expanded=False):
-        st.markdown("""Una vez activado, este modo simula el comportamiento de un caudalímetro electromagnético *real* incluyendo efectos típicos de instrumentación y electrónica.
-    
+        st.markdown("""Una vez activado, este modo simula el comportamiento de un caudalímetro electromagnético *real* incluyendo efectos típicos de instrumentación y electrónica. 
         **Modo de uso:**
         1. En el panel principal, active el **“Modo realista”**.
         2. Ajusta los parámetros (ruido, offset, deriva, no linealidad, cuantización, saturación e instalación).
@@ -514,7 +513,7 @@ En un caudalímetro electromagnético, el diámetro interno influye directamente
         - Cuantización simula resolución ADC.
         - Saturación recorta la señal a un máximo.
         - Instalación simula errores por perfiles de flujo/tramos rectos.
-                    """)
+        """)
 
 
 st.markdown(f"#### Configuración de Parámetros ({sistema})")
