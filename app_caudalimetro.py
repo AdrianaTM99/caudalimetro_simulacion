d24c3f2aa8267488738b0a88c88e864a8fcfed60
AdrianaTM99 <72172104+AdrianaTM99@users.noreply.github.com>
2026-05-08 22:12:06 -0400

    Update app_caudalimetro.py

--- app_caudalimetro.py
+++ app_caudalimetro.py
@@ -502,23 +502,23 @@ En un caudalímetro electromagnético, el diámetro interno influye directamente
     with st.expander('información sobre el **modo realista**', expanded=False):
             st.markdown("""Una vez activado, este modo simula el comportamiento de un caudalímetro electromagnético *real* incluyendo efectos típicos de instrumentación y electrónica.
             
-         **Modo de uso:**
+    **Modo de uso:**
          
-        1. En el panel principal, active el **“Modo realista”**.
-        2. Ajusta los parámetros (ruido, offset, deriva, no linealidad, cuantización, saturación e instalación).
-        3. Pulsa **“Generar curva de calibración”** para recalcular.
-        4. Si activas **“Mostrar curva ideal”**, verás dos curvas:
-           - **Ideal**: modelo físico sin imperfecciones.
-           - **Realista**: señal medida con errores y limitaciones.
+     1. En el panel principal, active el **“Modo realista”**.
+     2. Ajusta los parámetros (ruido, offset, deriva, no linealidad, cuantización, saturación e instalación).
+    3. Pulsa **“Generar curva de calibración”** para recalcular.
+    4. Si activas **“Mostrar curva ideal”**, verás dos curvas:
+    - **Ideal**: modelo físico sin imperfecciones.
+    - **Realista**: señal medida con errores y limitaciones.
 
            
-        **Interpretación:**
+    **Interpretación:**
         
-        - Ruido/offset/deriva afectan el “cero” y la dispersión.
-        - No linealidad introduce curvatura (la recta ya no ajusta perfecto).
-        - Cuantización simula resolución ADC.
-        - Saturación recorta la señal a un máximo.
-        - Instalación simula errores por perfiles de flujo/tramos rectos.
+    - Ruido/offset/deriva afectan el “cero” y la dispersión.
+    - No linealidad introduce curvatura (la recta ya no ajusta perfecto).
+    - Cuantización simula resolución ADC.
+    - Saturación recorta la señal a un máximo.
+    - Instalación simula errores por perfiles de flujo/tramos rectos.
                     
                     """)
 
